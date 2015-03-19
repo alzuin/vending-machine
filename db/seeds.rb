02c4565070c689336187f8a30e12f8aacb1466f3
Administrator <alberto@azns.it>
2015-03-19 11:54:05 +0000

    Seed coin database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed the coins
+
+[1,2,5,10,20,50,100,200].each do |coin_size|
+  Coin.create(size: coin_size , quantity: 0)
+end
